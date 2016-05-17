888eee46135f3a01ab822e6ef34125646ba3f278
paxromana96 <jack.brown.255@gmail.com>
2016-05-17 18:41:30 -0500

    Added documentation to syntax node helper functions and improved printing of syntax nodes. Also added more helper functions to determine whether a syntax node is an atom, and to print such atoms.

--- src/syntaxnode.h
+++ src/syntaxnode.h
@@ -6,15 +6,24 @@
 
 /**
  * This file describes a struct type representing a node in a syntax tree.
- * It also includes functiosn to generate and manipulate these nodes.
+ * It also includes functions to generate, manipulate, and print these nodes.
  */
 
 #define SYNTAX_CAR_TYPE_EMPTY -1
 #define SYNTAX_CAR_TYPE_TOKEN 1
 #define SYNTAX_CAR_TYPE_SYNTAX_NODE 3
+//The following two (_NUMBER and _STRING) are not yet implemented
+#define SYNTAX_CAR_TYPE_NUMBER 4
+#define SYNTAX_CAR_TYPE_STRING 5
 
 #define EMPTY_SYNTAX_NODE_CDR NULL
 
+/**
+ * A small struct that represents a single node of an abstract syntax tree,
+ * including both subtrees and atoms.
+ * Whether this node is an atom or a subtree, and what sort of atom it is,
+ * is determined by its carType.
+ */
 struct _syntaxnode {
 	union {
 		struct _syntaxnode * car;
@@ -26,30 +35,90 @@ struct _syntaxnode {
 
 typedef struct _syntaxnode syntaxnode;
 
+/**
+ * Allocates a block of memory that can hold num syntaxnodes using malloc(),
+ * and returns a pointer to the first one.
+ * This can be used to allocate a single value, or an array.
+ * These syntaxnodes are not initialized.
+ */
 syntaxnode * syntaxnodeAlloc(int num){
 	return (syntaxnode *) malloc( num * sizeof(syntaxnode));
 }
 
+/**
+ * Returns true if the given syntax node is the empty syntax node nil.
+ * A syntax node is determined to be nil if its carType is SYNTAX_CAR_TYPE_EMPTY.
+ *
+ * Precondition: node is not NULL
+ */
 int isEmptySyntaxNode(syntaxnode * node){
 	return node->carType == SYNTAX_CAR_TYPE_EMPTY;
 }
 
+/**
+ * Initializes the given syntaxnode node to nil, the empty node.
+ *
+ * Precondition: node is not NULL
+ */
 void initializeToEmptySyntaxNode(syntaxnode * node){
 	node->cdr = EMPTY_SYNTAX_NODE_CDR;
 	node->carType = SYNTAX_CAR_TYPE_EMPTY;
 }
 
+/**
+ * Allocates a single syntaxnode struct in heap memory,
+ * initializes it to nil (the empty syntax node), and 
+ * returns a pointer to it.
+ */
 syntaxnode * emptySyntaxnodeAlloc(){
 	syntaxnode * node = syntaxnodeAlloc(1);
 	initializeToEmptySyntaxNode(node);
 	return node;
 }
 
-syntaxnode * makeSyntaxTreeLeafFromToken(token * t){
+/**
+ * Allocates a syntax tree node that represents the given token t.
+ * The token is copied by pointer value, not by copying value.
+ * This node is considered an atom.
+ */
+syntaxnode * makeSyntaxTreeAtomFromToken(token * t){
 	syntaxnode * out = emptySyntaxnodeAlloc();
 	out->atom = t;
 	out->carType = SYNTAX_CAR_TYPE_TOKEN;
 	return out;
+}
+
+/**
+ * Returns true if the given syntax node is an atom or not.
+ * A syntax node is considered an atom if its carType is not
+ * another syntax node. In use, this means that this syntax node
+ * and its car are treated as the same thing; within the language,
+ * the car is the actual object being manipulated. Such an atom
+ * is represented as a syntax node for type consistency in the host
+ * language, C.
+ *
+ * For example, the number 5 in the language would be represented
+ * in this source code as a syntax node with a carType field of
+ * SYNTAX_CAR_TYPE_NUMBER and a value field of 5.
+ */
+int isSyntaxNodeAnAtom(syntaxnode * n){
+	return n == NULL || n->carType != SYNTAX_CAR_TYPE_SYNTAX_NODE;
+}
+
+/**
+ * Prints out the value of the syntaxnode n, which is assumed to be an atom, to f.
+ */
+fprintSyntaxnodeAtom(FILE * f, syntaxnode * n){
+	//Use a switch/case because of the enumerated car types like _TOKEN and _NUMBER
+	switch(n->carType){
+		case SYNTAX_CAR_TYPE_TOKEN:
+			fprintTokenText(f, *(n->atom));
+			break;
+		default:
+			fprintf(stderr, "%s\n", "Encountered syntaxnode atom with unexpected carType. Printing <Unexpected atom type>");
+			fprintf(f,"%s\n","<Unexpected atom type>");
+			break;
+	}
 }
 
 /**
@@ -60,28 +129,31 @@ void fprintSyntaxnode(FILE * f, syntaxnode * n){
 		fprintf(f,"nil");
 		return;
 	}
-	// fprintf(f,"%d:",n);
 
 	//print car
-	switch(n->carType){
-		case SYNTAX_CAR_TYPE_EMPTY:
-			//do nothing, assume end of list
-			fprintf(f,"nil");
-			return;
-		case SYNTAX_CAR_TYPE_TOKEN:
-			fprintTokenText(f, *(n->atom));
-			break;
-		case SYNTAX_CAR_TYPE_SYNTAX_NODE:
-			fprintf(f,"(");
-			fprintSyntaxnode(f, n->car);
-			fprintf(f,")");
-			break;
-	}
-	//print cdr
-	if(n->cdr != NULL){
-		fprintSyntaxnode(f, n->cdr);
+
+	if(isEmptySyntaxNode(n)){ //nil
+		//do nothing, assume end of list
+		fprintf(f,"nil");
+	} else if(isSyntaxNodeAnAtom(n)){
+		//just print its value
+		fprintSyntaxnodeAtom(f,n);
+	} else if(n->carType == SYNTAX_CAR_TYPE_SYNTAX_NODE){
+		fprintf(f,"(");
+		fprintSyntaxnode(f, n->car);
+		fprintf(f,")");
+		//print cdr
+		if(n->cdr != NULL){
+			fprintSyntaxnode(f, n->cdr);
+		}
+	} else {
+		fprintf(stderr, "%s\n", "Encountered syntaxnode non-atom of unexpected type. Printing <Unexpected non-atom type>");
+		fprintf(f,"%s\n","<Unexpected non-atom type>");
+		break;
 	}
 }
+
+
 
 void printSyntaxnode(syntaxnode * n){
 	fprintSyntaxnode(stdout, n);
