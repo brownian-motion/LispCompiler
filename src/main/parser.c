6fef4e3429f5a7c9cf98698f5fab11f881d7c43b
paxromana96 <jack.brown.255@gmail.com>
2016-05-18 14:36:15 -0500

    Refactored the parser and syntax tree generators to utilize the new tokenizer struct, which functions as a token stream for a file. Made some slight changes to function names to be more explicit.

--- src/main/parser.c
+++ src/main/parser.c
@@ -3,16 +3,6 @@
 #include "../parser.c"
 
 int main(int argc, char* argv []){
-	// struct parsenode lparen = makeAtom(getToken());
-	// struct parsenode one = makeEFromAtom(makeAtom(getToken()));
-	// struct parsenode two = makeEFromAtom(makeAtom(getToken()));
-	// struct parsenode three = makeEFromAtom(makeAtom(getToken()));
-	// struct parsenode rparen = makeAtom(getToken());
-	// struct parsenode emptyEs = makeEmptyEs();
-	// struct parsenode lastOne = makeEsFromList(three, emptyEs);
-	// struct parsenode lastTwo = makeEsFromList(two, lastOne);
-	// struct parsenode list = makeEFromList(lparen, one, lastTwo, rparen);
-	// struct parsenode program = makeProgram(list);
 	struct parsenode program;
 	if(buildParseTree(stdin, &program) == 0){
 		puts("Successful parsing!");
